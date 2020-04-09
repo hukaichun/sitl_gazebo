7b6696828bb8c7311953bcc5fa0b37de5153fe2c
hukaichun <hu.kaichun@gamil.com>
2020-04-09 21:31:32 +0800

    TRUE -> true

--- include/gazebo_opticalflow_plugin.h
+++ include/gazebo_opticalflow_plugin.h
@@ -13,79 +13,77 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
-*/
+ */
 #ifndef _GAZEBO_OPTICAL_FLOW_PLUGIN_HH_
 #define _GAZEBO_OPTICAL_FLOW_PLUGIN_HH_
 
 #include <string>
 
 #include "gazebo/common/Plugin.hh"
-#include "gazebo/sensors/ImuSensor.hh"
-#include "gazebo/sensors/CameraSensor.hh"
-#include "gazebo/gazebo.hh"
 #include "gazebo/common/common.hh"
-#include "gazebo/rendering/Camera.hh"
-#include "gazebo/util/system.hh"
-#include "gazebo/transport/transport.hh"
+#include "gazebo/gazebo.hh"
 #include "gazebo/msgs/msgs.hh"
 #include "gazebo/physics/physics.hh"
+#include "gazebo/rendering/Camera.hh"
+#include "gazebo/sensors/CameraSensor.hh"
+#include "gazebo/sensors/ImuSensor.hh"
+#include "gazebo/transport/transport.hh"
+#include "gazebo/util/system.hh"
 
 #include "OpticalFlow.pb.h"
 
-#include <opencv2/opencv.hpp>
-#include <iostream>
 #include <ignition/math.hh>
+#include <iostream>
+#include <opencv2/opencv.hpp>
 
 #include "flow_opencv.hpp"
 #include "flow_px4.hpp"
 
 #define DEFAULT_RATE 20
-#define HAS_GYRO TRUE
+#define HAS_GYRO true
 
 using namespace cv;
 using namespace std;
 
-namespace gazebo
-{
-  static const std::string kDefaultGyroTopic = "/px4flow/imu";
+namespace gazebo {
+static const std::string kDefaultGyroTopic = "/px4flow/imu";
 
-  class GAZEBO_VISIBLE OpticalFlowPlugin : public SensorPlugin
-  {
-    public:
-      OpticalFlowPlugin();
-      virtual ~OpticalFlowPlugin();
-      virtual void Load(sensors::SensorPtr _sensor, sdf::ElementPtr _sdf);
-      virtual void OnNewFrame(const unsigned char *_image,
-                              unsigned int _width, unsigned int _height,
-                              unsigned int _depth, const std::string &_format);
-      void ImuCallback(ConstIMUPtr& _imu);
+class GAZEBO_VISIBLE OpticalFlowPlugin : public SensorPlugin {
+ public:
+  OpticalFlowPlugin();
+  virtual ~OpticalFlowPlugin();
+  virtual void Load(sensors::SensorPtr _sensor, sdf::ElementPtr _sdf);
+  virtual void OnNewFrame(const unsigned char *_image, unsigned int _width,
+                          unsigned int _height, unsigned int _depth,
+                          const std::string &_format);
+  void ImuCallback(ConstIMUPtr &_imu);
 
-    protected:
-      unsigned int width, height, depth;
-      std::string format;
-      sensors::CameraSensorPtr parentSensor;
-      rendering::CameraPtr camera;
-      physics::WorldPtr world;
+ protected:
+  unsigned int width, height, depth;
+  std::string format;
+  sensors::CameraSensorPtr parentSensor;
+  rendering::CameraPtr camera;
+  physics::WorldPtr world;
 
-    private:
-      event::ConnectionPtr newFrameConnection;
-      transport::PublisherPtr opticalFlow_pub_;
-      transport::NodePtr node_handle_;
-      transport::SubscriberPtr imuSub_;
-      sensor_msgs::msgs::OpticalFlow opticalFlow_message;
-      ignition::math::Vector3d opticalFlow_rate;
-      std::string namespace_;
-      std::string gyro_sub_topic_;
-      OpticalFlowOpenCV *optical_flow_;
-      // OpticalFlowPX4 *optical_flow_;
+ private:
+  event::ConnectionPtr newFrameConnection;
+  transport::PublisherPtr opticalFlow_pub_;
+  transport::NodePtr node_handle_;
+  transport::SubscriberPtr imuSub_;
+  sensor_msgs::msgs::OpticalFlow opticalFlow_message;
+  ignition::math::Vector3d opticalFlow_rate;
+  std::string namespace_;
+  std::string gyro_sub_topic_;
+  OpticalFlowOpenCV *optical_flow_;
+  // OpticalFlowPX4 *optical_flow_;
 
-      float hfov_;
-      int dt_us_;
-      int output_rate_;
-      float focal_length_;
-      double first_frame_time_;
-      uint32_t frame_time_us_;
-      bool has_gyro_;
-  };
-}
+  float hfov_;
+  int dt_us_;
+  int output_rate_;
+  float focal_length_;
+  double first_frame_time_;
+  uint32_t frame_time_us_;
+  bool has_gyro_;
+};
+}  // namespace gazebo
 #endif
